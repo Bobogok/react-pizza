9d8e58164a8595e9bd9447a8cd2de014f2606a45
Ivan M <vania.morskov@yandex.ru>
2021-10-01 20:51:26 +0300

    add SortPopup component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Header from './Header';
 import Categories from './Categories';
+import SortPopup from './SortPopup';
 
-export { Categories, Header };
+export { Categories, Header, SortPopup };
