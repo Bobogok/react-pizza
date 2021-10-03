957cd451b75454e15af7ee04e915f6c32166082b
Ivan M <vania.morskov@yandex.ru>
2021-10-03 16:00:58 +0300

    update folder styles, change path in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './styles/index.scss';
+import './styles/libs/index.scss';
 
 import App from './App';
 
