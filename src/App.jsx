b8c81d0692ff0e8e1f4027fe33034ff6a5dc9b54
Ivan M <vania.morskov@yandex.ru>
2021-10-05 15:56:09 +0300

    add hook useDispatch in App.jsx, add useSelector in Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
 
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
-function App({ items, setPizzas }) {
+function App() {
+  // This hook returns a reference to the dispatch function from the Redux store.
+  const dispatch = useDispatch();
+
   useEffect(() => {
     axios
       .get('http://localhost:1337/db.json')
       .then(({ data }) => {
-        setPizzas(data.pizzas);
+        console.log(data);
+        dispatch(setPizzasAction(data.pizzas));
       })
       .catch((err) => {
         console.error(err);
@@ -24,26 +28,11 @@ function App({ items, setPizzas }) {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" render={() => <Home items={items} />} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
   );
 }
 
-// will be merged as props to App component
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items
-  };
-};
-
-// will be given the dispatch setPizzas of store
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setPizzas: (items) => dispatch(setPizzasAction(items))
-  };
-};
-
-// we subs at changes in store, if store is changed - App rerendering
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Categories, SortPopup, Card } from '../components';
 
-function Home({ items }) {
+function Home() {
+  // The selector will be called with the entire Redux store state as its only argument.
+  // The selector will be run whenever the function component renders
+  // (unless its reference hasn't changed since a previous render of the component
+  // so that a cached result can be returned by the hook without re-running the selector).
+  // useSelector() will also subscribe to the Redux store, and run your selector
+  // whenever an action is dispatched.
+  const { items } = useSelector(({ pizzas }) => {
+    console.log(pizzas);
+    return {
+      items: pizzas.items
+    };
+  });
+
   return (
     <div className="container">
       <div className="content__top">
@@ -15,11 +29,7 @@ function Home({ items }) {
         />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {items.map((obj) => (
-          <Card key={obj.id} {...obj} />
-        ))}
-      </div>
+      <div className="content__items">{items && items.map((obj) => <Card key={obj.id} {...obj} />)}</div>
     </div>
   );
 }
