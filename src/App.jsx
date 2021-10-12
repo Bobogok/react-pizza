40d3801687dd3f2d911bc4463795c0d88845873c
Ivan M <vania.morskov@yandex.ru>
2021-10-12 12:07:52 +0300

    new merge changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import useLoadPizza from './hooks/useLoadPizza';
-<<<<<<< HEAD
 import useLoadCart from './hooks/useLoadCart';
-=======
->>>>>>> dc5c3600a0e580c2e47e895924f58036c10fdd7b
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
   const { category, sortBy } = useLoadPizza();
-<<<<<<< HEAD
   const { rowEmptyCart, rowCompleteCart } = useLoadCart();
-=======
->>>>>>> dc5c3600a0e580c2e47e895924f58036c10fdd7b
 
   return (
     <div className="wrapper">
@@ -24,11 +18,7 @@ function App() {
           <Home category={category} sortBy={sortBy} />
         </Route>
         <Route exact path="/cart">
-<<<<<<< HEAD
           <Cart emptyCart={rowEmptyCart} completeCart={rowCompleteCart} />
-=======
-          <Cart />
->>>>>>> dc5c3600a0e580c2e47e895924f58036c10fdd7b
         </Route>
       </div>
     </div>
